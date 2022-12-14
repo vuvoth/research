b9b8df0a31c363b1908a85b600fcd6731d534799
Vu Vo <vu.voth@gmail.com>
2022-12-14 17:04:00 +0700

    try poseidon hash

--- semaphore/halo2-semaphore/src/main.rs
+++ semaphore/halo2-semaphore/src/main.rs
@@ -1,3 +1,9 @@
+use halo2curves::pasta::pallas;
+use halo2_gadgets::poseidon::primitives::{P128Pow5T3, Hash, ConstantLength};
+
 fn main() {
-    println!("Hello, world!");
+    let message = [pallas::Base::from(6), pallas::Base::from(42)];
+    let hasher = Hash::<_, P128Pow5T3, ConstantLength<2>, 3, 2>::init();
+    let result = hasher.hash(message);
+    println!("{:#?}", result);
 }
